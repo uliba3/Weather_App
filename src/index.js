3cd5cdef91f68711c59cc8d56627e681bd40fc26
uliba3 <60570852+ulilba3@users.noreply.github.com>
2023-09-19 09:20:31 +0900

    add weather informations

--- src/index.js
+++ src/index.js
@@ -1,33 +1,54 @@
-// index.js
+// dist/index.js
 import { getWeather } from "./api.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     
     const weatherForm = document.getElementById("weather-form");
+    const weatherLocationInput = document.getElementById("weather-location");
+    const country = document.getElementById("country");
+    const city = document.getElementById("city");
+    const weatherName = document.getElementById("weather-name");
+    const weatherTime = document.getElementById("weather-time");
+    const weatherIcon = document.getElementById("weather-icon");
+    const temperatureDegree = document.getElementById("temperatureDegree");
+    const windDirection = document.getElementById("windDirection");
+    const windDegree = document.getElementById("windDegree");
+
+    const isCelsius = true; 
 
     weatherForm.addEventListener("submit", async (event) => {
         event.preventDefault();
 
-        
-        const weatherLocationInput = document.getElementById("weather-location");
-        const weatherIcon = document.getElementById("weather-icon");
-        const weatherName = document.getElementById("weather-name");
-        
-
         const location = weatherLocationInput.value;
-        console.log(location);
 
         try {
             const weatherData = await getWeather(location);
             console.log(weatherData); // You can process the data here
 
-            // Extract the relevant data from the response
-            const weatherIconUrl = weatherData.current.condition.icon;
-            const weatherText = weatherData.current.condition.text;
+            const weather = {
+                country: weatherData.location.country,
+                city: weatherData.location.region,
+                state: weatherData.current.condition.text,
+                lastUpdated: weatherData.current.last_updated,
+                iconUrl: weatherData.current.condition.icon,
+                humidity: weatherData.current.humidity,
+                temperatureC: weatherData.current.temp_c,
+                temperatureF: weatherData.current.temp_f,
+                windDirection: weatherData.current.wind_dir,
+                windKph: weatherData.current.wind_kph,
+                windMph: weatherData.current.wind_mph,
+            }
 
             // Update the DOM with the weather data
-            weatherIcon.src = weatherIconUrl;
-            weatherName.textContent = weatherText;
+            weatherIcon.src = weather.iconUrl;
+            country.innerText = weather.country;
+            city.innerText = weather.city;
+            weatherName.innerText = weather.state;
+            weatherTime.innerText = weather.lastUpdated;
+            temperatureDegree.innerText = weather.temperatureC;
+            windDirection.innerText = weather.windDirection;
+            windDegree.innerText = weather.windKph;
+            
         } catch (error) {
             console.error(error);
         }
